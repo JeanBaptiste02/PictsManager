360653daeeeadfa491d2f9e0266075f390ce4764
DAMODARANE Jean-Baptiste <91695685+JeanBaptiste02@users.noreply.github.com>
2024-03-19 00:12:36 +0100

    initiated backend development

---
+++ src/main/java/com/epitech/pictsmanager/PictsmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.epitech.pictsmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PictsmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PictsmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epitech/pictsmanager/PictsmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epitech.pictsmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PictsmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
