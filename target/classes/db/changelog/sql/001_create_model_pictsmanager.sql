f6c93c06b489f7eafe9125b46d75e11255e660ed
DAMODARANE Jean-Baptiste <91695685+JeanBaptiste02@users.noreply.github.com>
2024-03-28 22:47:18 +0100

    successfully implemented liquibase system

--- target/classes/db/changelog/sql/001_create_model_pictsmanager.sql
+++ target/classes/db/changelog/sql/001_create_model_pictsmanager.sql
@@ -5,28 +5,27 @@ CREATE TABLE IF NOT EXISTS users (
     password VARCHAR(255) NOT NULL
 );
 
-
-CREATE TABLE IF NOT EXISTS public."Albums" (
+CREATE TABLE IF NOT EXISTS albums (
     id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     owner_id INTEGER NOT NULL,
-    CONSTRAINT fk_owner_id FOREIGN KEY (owner_id) REFERENCES public."Users" (id) ON UPDATE NO ACTION ON DELETE NO ACTION
+    CONSTRAINT fk_owner_id_albums FOREIGN KEY (owner_id) REFERENCES users (id)
 );
 
-CREATE TABLE IF NOT EXISTS public."Photos" (
+CREATE TABLE IF NOT EXISTS photos (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     album_id INTEGER NOT NULL,
     owner_id INTEGER NOT NULL,
-    CONSTRAINT fk_album_id FOREIGN KEY (album_id) REFERENCES public."Albums" (id) ON UPDATE NO ACTION ON DELETE NO ACTION,
-    CONSTRAINT fk_owner_id_photos FOREIGN KEY (owner_id) REFERENCES public."Users" (id) ON UPDATE NO ACTION ON DELETE NO ACTION
+    CONSTRAINT fk_album_id_photos FOREIGN KEY (album_id) REFERENCES albums (id),
+    CONSTRAINT fk_owner_id_photos FOREIGN KEY (owner_id) REFERENCES users (id)
 );
 
-CREATE TABLE IF NOT EXISTS public."AccessRights" (
+CREATE TABLE IF NOT EXISTS access_rights (
     id SERIAL PRIMARY KEY,
     user_id INTEGER NOT NULL,
     album_id INTEGER NOT NULL,
-    CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES public."Users" (id) ON UPDATE NO ACTION ON DELETE NO ACTION,
-    CONSTRAINT fk_album_id_accessrights FOREIGN KEY (album_id) REFERENCES public."Albums" (id) ON UPDATE NO ACTION ON DELETE NO ACTION,
+    CONSTRAINT fk_user_id_access_rights FOREIGN KEY (user_id) REFERENCES users (id),
+    CONSTRAINT fk_album_id_access_rights FOREIGN KEY (album_id) REFERENCES albums (id),
     CONSTRAINT unique_user_album UNIQUE (user_id, album_id)
 );
--- target/classes/db/changelog/sql/001_insert_datas_pictsmanager.sql
+++ target/classes/db/changelog/sql/001_insert_datas_pictsmanager.sql
@@ -1,23 +1,19 @@
--- Insérer des utilisateurs
-INSERT INTO public."Users" (nom, email, password) VALUES
-                                                      ('John Doe', 'john@example.com', 'password123'),
-                                                      ('Alice Smith', 'alice@example.com', 'securepass'),
-                                                      ('Bob Johnson', 'bob@example.com', 'bobspassword');
+INSERT INTO users (nom, email, password) VALUES
+                                                      ('Ganesh', 'john@example.com', 'password123'),
+                                                      ('Ramesh', 'alice@example.com', 'securepass'),
+                                                      ('Sri Ram', 'bob@example.com', 'bobspassword');
 
--- Insérer des albums
-INSERT INTO public."Albums" (title, owner_id) VALUES
+INSERT INTO albums (title, owner_id) VALUES
                                                   ('Vacances été 2023', 1),
                                                   ('Photos de famille', 2),
-                                                  ('Voyage à Paris', 3);
+                                                  ('Voyage à Mumbai', 3);
 
--- Insérer des photos
-INSERT INTO public."Photos" (name, album_id, owner_id) VALUES
+INSERT INTO photos (name, album_id, owner_id) VALUES
                                                            ('Plage', 1, 1),
                                                            ('Anniversaire', 2, 2),
                                                            ('Tour Eiffel', 3, 3);
 
--- Insérer des droits d'accès
-INSERT INTO public."AccessRights" (user_id, album_id) VALUES
-                                                          (2, 1), -- Alice a accès aux photos de John
-                                                          (3, 2), -- Bob a accès aux photos d'Alice
-                                                          (1, 3); -- John a accès aux photos de Bob
+INSERT INTO access_rights (user_id, album_id) VALUES
+                                                           (2, 1),
+                                                           (3, 2),
+                                                           (1, 3);
