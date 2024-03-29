c4fcbf5f4ddb9d87625f08bcce2c41de7fe00409
DAMODARANE Jean-Baptiste <91695685+JeanBaptiste02@users.noreply.github.com>
2024-03-29 23:48:11 +0100

    done photo form

--- src/main/java/com/epitech/pictsmanager/form/PhotoForm.java
+++ src/main/java/com/epitech/pictsmanager/form/PhotoForm.java
@@ -1,4 +1,28 @@
 package com.epitech.pictsmanager.form;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PhotoForm {
+    @JsonProperty("name")
+    private String name;
+    @JsonProperty("path")
+    private String path;
+    @JsonProperty("description")
+    private String description;
+    @JsonProperty("date")
+    private LocalDateTime date;
+    @JsonProperty("album_id")
+    private Long album_id;
+    @JsonProperty("owner_id")
+    private Long owner_id;
 }
